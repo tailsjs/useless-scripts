d1acf16bf1e745c1642976617f276550430bb945
tailsjs <49571184+tailsjs@users.noreply.github.com>
2020-07-13 11:48:11 +0300

    Create simple_bot.js

---
+++ simple_bot.js
@@ -0,0 +1,25 @@
+// бот основанный на vk-io гайд: vk.com/@tailsjsundefined-bot-na-js-blya
+let VK = require('vk-io')
+let vk = new VK({
+token: 'токен пизды',
+group_id: айдигруппы
+})
+
+vk.updates.startPolling()
+
+vk.updates.on(['new_message', 'edit_message'], async(ctx) => {
+
+if (ctx.senderId < 1 || ctx.isOutbox) { 
+return
+}
+ctx.setActivity()
+
+//мне лень писать эту систему команд поэтому на ифах
+
+if(ctx.text == 'привет'){
+
+ctx.send('ЕБАААТЬ!!!! ЭТО ЖЕ Я!')
+
+}
+
+})
