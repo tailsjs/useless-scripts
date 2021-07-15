6df921291c451c6ad8815cba55f6b9cb654f99f4
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-07-15 23:05:25 +0400

    yagul py

---
+++ ghoul.py
@@ -0,0 +1,13 @@
+"""
+    Ghoul 1000-7 script.
+    Subtract 7 from 1000 until you get -1.
+    JS Version used by me in this video: https://www.youtube.com/watch?v=o0xGL3aK-H8
+    Just a meme script, don't take it seriously.
+    Plus, I haven't published anything to this repository for a whole year.
+    PORT TO PYTHON.
+"""
+
+i = 1000
+while i > 0:
+    print(f"{i}-7={i-7}")
+    i-=7
