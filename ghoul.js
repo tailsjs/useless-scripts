55411d6a8d0696e56c7f303288448003886d8bff
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-07-15 23:00:33 +0400

    ya gul

---
+++ ghoul.js
@@ -0,0 +1,12 @@
+/*
+** Ghoul 1000-7 script.
+** Subtract 7 from 1000 until you get -1.
+** Used by me in this video: https://www.youtube.com/watch?v=o0xGL3aK-H8
+** Just a meme script, don't take it seriously.
+** Plus, I haven't published anything to this repository for a whole year.
+*/
+i = 1000;
+do{
+    console.log(`${i}-7=${i-7}`);
+    i-=7
+}while(i>0)
