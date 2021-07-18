1ee9a49c27fa9a6cc831a826a2e8de3ea7d35a2e
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-07-18 21:17:12 +0400

    terexcc govnoed!!!

---
+++ callback-example.js
@@ -0,0 +1,16 @@
+/*
+** Callback Example
+** For noob-coders
+*/
+
+function helloworld(name, callback){
+    if (!name) return callback({ ok: false, response: "Where's your name?" });
+    const hello = `Hello world! From ${name}!`;
+
+    return callback({ ok: true, response: hello });
+};
+
+helloworld("tails", function(res){
+    if(!res.ok)return console.log(res.response);
+    return console.log(res.response);
+});
