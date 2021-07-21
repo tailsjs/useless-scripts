0f35904fde2a51d2a52947223441adb24035645c
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-07-21 13:55:34 +0400

    Update download_img.js

--- download_img.js
+++ download_img.js
@@ -1,6 +1,6 @@
 /*
 ** Download picture by URL.
-** Used in: screenshot-site
+** Used in: screenshot-site 2.0.0, 2.1.0.
 ** Need to install: fs, request
 ** Useless fact: Foxes live an average of four years in an urban setting and usually breed from January to March
 */
