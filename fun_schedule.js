561aa0e0177f1529c8bb6eec5287e1609ba1500c
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-07-22 21:34:19 +0400

    Update fun_schedule.js

--- fun_schedule.js
+++ fun_schedule.js
@@ -7,7 +7,5 @@ function random(min, max) {return Math.round(Math.random() * (max - min)) + min}
 
 let array = "wake up,go to school,teach js,play cs:go with onetap,become boomer".split(',') // comma separated actions
 let string = ``
-for(let i = 0; i < random(3,13); i++){
-string += `${i + 1}. ${array[random(0,array.length - 1)]}\n`
-}
+for(let i = 0; i < random(3,13); i++)string += `${i + 1}. ${array[random(0,array.length - 1)]}\n`
 return console.log(string)
