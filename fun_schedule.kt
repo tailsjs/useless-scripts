a0d181f2eef5ed5b713797a2d4560a3834d1952f
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-07-22 23:26:54 +0400

    terxcc govnoed!

---
+++ fun_schedule.kt
@@ -0,0 +1,22 @@
+/*
+** Randomly generates a fun schedule.
+** PORT TO KOTLIN
+*/
+
+import java.util.Random
+
+val random = Random()
+
+fun main() {
+    val maxNumber: Int = rand(3, 10)
+    val strings = "wake up,go to school,teach kotlin,play cs:go with onetap,become boomer".split(",").toTypedArray()
+    var done = ""
+    for(i in 1..maxNumber){
+        done += "$i. ${strings[rand(0, strings.size)]}\n"
+    }
+    return print(done)  
+}
+
+fun rand(from: Int, to: Int) : Int {
+    return random.nextInt(to - from) + from
+}
