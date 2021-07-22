95de7ce535c5cc3aafcc9dbf24c36545d39e0b5d
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-07-23 00:38:46 +0400

    Update download_img.js

--- download_img.js
+++ download_img.js
@@ -8,7 +8,7 @@
 const fs = require("fs");
 const request = require("request");
 const uri = "https://i.pinimg.com/originals/e6/03/ec/e603ec55a0a100750c65c10b396f1206.png";
-const fileName = "./cute_fox.png"
+const fileName = "./cute_fox.png";
 
 request(uri).pipe(fs.createWriteStream(fileName)).on('close', function(){
     return console.log('Done!')
