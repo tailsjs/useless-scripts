b431824122e843e8105fea189fe60640ff52d7a1
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-07-22 23:11:45 +0400

    ._.

---
+++ ghoul.kt
@@ -0,0 +1,17 @@
+/*
+**  Ghoul 1000-7 script.
+**  Subtract 7 from 1000 until you get -1.
+**  JS Version used by me in this video: https://www.youtube.com/watch?v=o0xGL3aK-H8
+**  Just a meme script, don't take it seriously.
+**  Plus, I haven't published anything to this repository for a whole year.
+**  PORT TO KOTLIN. (Yes, I started learning Kotlin.)
+**  I think, you know how to build Kotlin script. [when I fix my computer. I will make a build especially for you.]
+*/
+
+fun main() {
+    var i: Int = 1000
+    do {
+        println("$i-7=${i-7}")
+        i-=7
+    } while(i > 0)
+}
