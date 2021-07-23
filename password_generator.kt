d8c75b42f2663985bbdb6f376e46d784ce9c87b7
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-07-23 19:02:54 +0400

    Create password_generator.kt

---
+++ password_generator.kt
@@ -0,0 +1,23 @@
+/*
+** PASSWORD GENERATOR
+**
+** Kinda useless, because in internet you can find other scripts with same use, but it's my repo, it's my rules.
+*/
+
+import java.util.Random
+
+val random = Random()
+
+fun main() {
+	val letters: String = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789!@#$%^&*_"
+    val passLength: Int = 10
+    var result: String = ""
+    
+    for(i in 1..passLength)result += letters[rand(0, letters.length)]
+    
+    println(result)
+}
+
+fun rand(from: Int, to: Int) : Int {
+    return random.nextInt(to - from) + from
+}
