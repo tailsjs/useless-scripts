3ed94d9dd913f3338ae1c9a6ad9d671efbe89598
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-07-23 18:50:28 +0400

    Create password_generator.js

---
+++ password_generator.js
@@ -0,0 +1,14 @@
+/*
+** PASSWORD GENERATOR
+**
+** Kinda useless, because in internet you can find other scripts with same use, but it's my repo, it's my rules.
+*/
+
+const letters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789!@#$%^&*_"; // Your letters
+const passLength = 10; // Your password length
+let result = ""; // The variable where our generated password will be stored.
+for(let i = 0; i < passLength; i++)result += letters[random(0, letters.length)];
+
+return console.log(result);
+
+function random(min, max) {return Math.round(Math.random() * (max - min)) + min};
