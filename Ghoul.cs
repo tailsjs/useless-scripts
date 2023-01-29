9179c2302171a688c7cfc30c43aff4d7644256e1
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-01-30 00:33:23 +0400

    yagul c#

---
+++ Ghoul.cs
@@ -0,0 +1,15 @@
+﻿namespace Ghoul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1000;
+            do
+            {
+                Console.WriteLine($"{i}-7={i - 7}");
+                i -= 7;
+            } while (i > 0);
+        }
+    }
+}
