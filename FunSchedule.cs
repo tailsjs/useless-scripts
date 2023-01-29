21bbcf0d7fa547d31af4cdb16358ea6cfaa98116
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-01-30 00:30:25 +0400

    funschedule via c#

---
+++ FunSchedule.cs
@@ -0,0 +1,17 @@
+﻿namespace FunSchedule
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random rng = new Random();
+            string[] ListOfNeed = "wake up,go to school,teach c#,play cs:go with onetap,become boomer".Split(",");
+            string result = "";
+            for(var i = 0; i < rng.Next(3, 13); i++)
+            {
+                result += $"{i + 1}. {ListOfNeed[rng.Next(ListOfNeed.Length)]}\n";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
