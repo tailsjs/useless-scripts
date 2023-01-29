c61f821808856b620b9c79910662ec775e6655a9
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-01-30 00:22:17 +0400

    passgen ept c#

---
+++ PasswordGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace PassGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string Symbols = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789!@#$%^&*_";
+            int passLength = 10;
+            string result = "";
+
+            for(var i = 0; i < passLength; i++)
+            {
+                result += Symbols[rnd.Next(Symbols.Length)];
+            };
+            Console.WriteLine(result);
+            Console.WriteLine("Press ENTER");
+            Console.ReadLine(); // wow
+        }
+    }
+}
