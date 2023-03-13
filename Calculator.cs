fe991da3c92e678b6e809807f2bd0c2251362b9b
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-03-13 18:53:36 +0400

    калл кулятор

---
+++ Calculator.cs
@@ -0,0 +1,48 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введи первое число:");
+                float Value1 = Convert.ToSingle(Console.ReadLine().Replace(".", ","));
+
+
+                Console.WriteLine("Теперь введи оператор (+, -, /, *):");
+                string Operator = Console.ReadLine();
+
+                Console.WriteLine("Теперь введи второе число:");
+                float Value2 = Convert.ToSingle(Console.ReadLine().Replace(".", ","));
+
+                float result = 0;
+                switch (Operator)
+                {
+                    case "+":
+                        result = Value1 + Value2;
+                        break;
+                    case "-":
+                        result = Value1 + Value2;
+                        break;
+                    case "/":
+                        result = Value1 / Value2;
+                        break;
+                    case "*":
+                        result = Value1 * Value2;
+                        break;
+                    default:
+                        Console.WriteLine("Такого оператора нет! Выдаём 0...");
+                        result = 0;
+                        break;
+                }
+
+                Console.WriteLine($"Результат: {result}");
+            } catch (Exception)
+            {
+                Console.WriteLine("Ой... Произошла ошибка...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
